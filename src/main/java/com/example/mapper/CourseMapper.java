8a4a804e6b9af71ceb7797d7feab21c6114c01c6
yuugrid <152932000+yuugrid@users.noreply.github.com>
2024-02-20 08:46:12 +0900

    FirstCommit

---
+++ src/main/java/com/example/mapper/CourseMapper.java
@@ -0,0 +1,12 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.entity.Course;
+
+@Mapper
+public interface CourseMapper {
+	public List<Course> findAll();
+}
